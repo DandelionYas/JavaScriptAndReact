1eff0114add9c72a196f0d33f8cc220b9edaf8cd
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2022-02-09 13:24:06 +0330

    Add useful comment

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ superman.sayHello();
 
 var failThis = superman.sayHello;
  
+//calling failThis in the global scope outputs "Hello, I'm undifined."
 //This is because the variable failThis belongs to the global scope, which contains no member variable named heroName
 failThis();
 
 //In Javascript the value of the this keyword is completely overridable to be anything that we want!
+//The code below override this with superman
 failThis.call(superman);
